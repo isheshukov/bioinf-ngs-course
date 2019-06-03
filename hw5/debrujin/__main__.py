29cba31b245777a24f7f97393d7f665d314d8cce
Ilia Sheshukov <sheshukov.ilya@gmail.com>
2019-06-03 03:51:50 +0300

    removed large files

--- hw5/debrujin/__main__.py
+++ hw5/debrujin/__main__.py
@@ -36,7 +36,7 @@ def fasta2graph(fasta, k):
 
     for l, r in tqdm(product(G, repeat=2), desc="Creating edges"):
         if l[1:] == r[:-1]:
-            G.add_edge(l, r, seq=l+r[-1])
+            G.add_edge(l, r, seq=l+r[-1], length=len(l+r[-1]))
 
 
     return G, coverage
@@ -54,7 +54,8 @@ def compress_graph(G, k):
                 G.remove_edge(in_node, node)
                 G.remove_edge(node, out_node)
                 nodes_to_remove.append(node)
-                G.add_edge(in_node, out_node, seq=in_data['seq']+out_data['seq'])
+                new_seq = in_data['seq'] + out_data['seq'][k:]
+                G.add_edge(in_node, out_node, seq=new_seq, length=len(new_seq))
 
         G.remove_nodes_from(nodes_to_remove)
         if num_of_nodes == len(G):
@@ -66,12 +67,44 @@ def compute_mean_coverage(G, coverage, k):
     def seq_to_mean(seq, k):
         return mean(list(map(lambda x: coverage[''.join(x)], windowed(seq, k))))
 
-    mean_dict = {}
+    labels_dict = {}
+    mean_cov = {}
+    length = {}
 
     for e in tqdm(G.edges(data=True, keys=True), desc="Computing mean coverage"):
-        mean_dict[(e[0],e[1],e[2])] = {'mean cov': seq_to_mean(e[3]['seq'], k)}
+        mean_cov[(e[0],e[1],e[2])] = seq_to_mean(e[3]['seq'], k)
+        length[(e[0],e[1],e[2])] = len(e[3]['seq'])
+
+        labels_dict[(e[0],e[1],e[2])] = f"mean_cov: {seq_to_mean(e[3]['seq'], k):0.3}\n length: {e[3]['length']}"
     
-    nx.set_edge_attributes(G, mean_dict, 'mean cov')
+    nx.set_edge_attributes(G, labels_dict, 'label')
+    nx.set_edge_attributes(G, mean_cov, 'mean_cov')
+    nx.set_edge_attributes(G, length, 'length')
+
+def remove_bad_edges(G, coverage, k):
+    remove_threshold = 2*k
+
+    edges_to_remove = []
+    for e in tqdm(G.edges(data=True, keys=True), desc="Finding bad edges"):
+        if e[3]['mean_cov'] < 30 or e[3]['length'] < remove_threshold:
+            edges_to_remove.append(e) 
+
+    G.remove_edges_from(edges_to_remove)
+
+def remove_dead_ends(G, coverage, k):
+    threshold = 2*k
+    edges_to_remove = []
+    for node in tqdm(G, desc="Compressing"):
+        if G.out_degree(node) == 0:
+            in_node, _, in_data = list(G.in_edges(node, data=True))[0]
+
+            edge_data = G.get_edge_data(in_node, node)
+            #print(edge_data)
+            if 'length' in edge_data:
+                if (edge_data['length'] < threshold):
+                    edges_to_remove.append((in_node, node))
+
+    G.remove_edges_from(edges_to_remove)
 
 def main(fasta, k=3):
     """Runs de Brujin graph building algorithm """
@@ -81,6 +114,8 @@ def main(fasta, k=3):
     dbg, coverage = fasta2graph(fasta, k)
     compress_graph(dbg, k)
     compute_mean_coverage(dbg, coverage, k)
+    #remove_bad_edges(dbg, coverage, k)
+    remove_dead_ends(dbg, coverage, k)
 
     dbg = nx.relabel.convert_node_labels_to_integers(dbg)
     #dbg = compress_graph(dbg)
